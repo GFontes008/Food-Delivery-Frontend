06af5f883fcdde4b7dd9e661db5ce98dc5c02f29
Ana Rosa <guilherme.fontes98@outlook.com>
2024-07-02 15:30:47 +0100

    done style

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,6 @@ const Home = () => {
     <div className="homepage">
       <aside className="title">
         <h1>Welcome {user ? user.name : "Food Lover"}!</h1>
-        <p>
-          to DELIVERY, where delicious meals are just a tap away. Discover our
-          wide range of cuisines and experience the joy of fast, reliable
-          delivery. Craving something special? Try what we have to offer and
-          indulge in the finest flavors delivered right to your doorstep.
-        </p>
       </aside>
       <div className="content">
         <div className="foodlist">
